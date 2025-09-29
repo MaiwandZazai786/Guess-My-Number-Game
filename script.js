6a601fb336924bbb9b7f59bc58f0c8c51e1fb9ee
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2025-09-30 00:12:05 +0200

    getting and checking the userInput

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 'use strict';
 
 const secretNumber = Math.floor(Math.random() * 20 + 1);
+let message = document.querySelector('.message');
+let currentScore = 20;
+const score = document.querySelector('.score');
+const checkBtn = document.querySelector('.check');
+ 
+checkBtn.onclick = () => {
+    const userGuess = Number(document.querySelector('.guess').value);
+    if (userGuess > secretNumber)
+    {
+        message.textContent = 'Too High';
+        currentScore--;
+        score.textContent = currentScore;
+    }
+    else if (userGuess < secretNumber)
+    {
+        message.textContent = 'Too Low';
+         currentScore--;
+        score.textContent = currentScore;
+    }
+    else {
+        message.textContent = 'Right number! you Win...🥳';
+    }
+}
+

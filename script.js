779c9f7254d3833b5dcf08fe076fb79e7cfd4644
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2025-09-30 00:13:34 +0200

    fixed

--- script.js
+++ script.js
@@ -5,23 +5,18 @@ let message = document.querySelector('.message');
 let currentScore = 20;
 const score = document.querySelector('.score');
 const checkBtn = document.querySelector('.check');
- 
-checkBtn.onclick = () => {
-    const userGuess = Number(document.querySelector('.guess').value);
-    if (userGuess > secretNumber)
-    {
-        message.textContent = 'Too High';
-        currentScore--;
-        score.textContent = currentScore;
-    }
-    else if (userGuess < secretNumber)
-    {
-        message.textContent = 'Too Low';
-         currentScore--;
-        score.textContent = currentScore;
-    }
-    else {
-        message.textContent = 'Right number! you Win...🥳';
-    }
-}
 
+checkBtn.onclick = () => {
+  const userGuess = Number(document.querySelector('.guess').value);
+  if (userGuess > secretNumber) {
+    message.textContent = 'Too High';
+    currentScore--;
+    score.textContent = currentScore;
+  } else if (userGuess < secretNumber) {
+    message.textContent = 'Too Low';
+    currentScore--;
+    score.textContent = currentScore;
+  } else {
+    message.textContent = 'Right number! you Win...🥳';
+  }
+};

ab01980ee61f5c0c49e2bcdc5e3b37b0d7d3f1c0
Maiwand Zazai <Maiwand.Zazai77@gmail.com>
2025-10-03 23:51:52 +0200

    check if the user already guessed the number

--- script.js
+++ script.js
@@ -6,30 +6,47 @@ const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const bodyElemetn = document.querySelector('body');
 const number = document.querySelector('.number');
+let alreadyGuessed = false;
 
 const checkBtn = document
   .querySelector('.check')
   .addEventListener('click', () => {
-    const userGuess = Number(document.querySelector('.guess').value);
-    if (!userGuess) {
-      message.textContent = 'No Number ⛔';
+    // check if the user already guessed the number
+    if (alreadyGuessed) {
+      message.textContent = 'You alerdy gussed the number :)';
+    }
+    // check if the user has left chances
+    else if (currentScore < 1) {
+      message.textContent = 'You lost, Game over';
     } else {
-      if (userGuess > secretNumber) {
-        message.textContent = 'Too High 💹';
-        currentScore--;
-        score.textContent = currentScore;
-      } else if (userGuess < secretNumber) {
-        message.textContent = 'Too Low 💹';
-        currentScore--;
-        score.textContent = currentScore;
+      const userGuess = Number(document.querySelector('.guess').value);
+      // when there is no number
+      if (!userGuess) {
+        message.textContent = 'No Number ⛔';
       } else {
-        message.textContent = 'Correct Number! 🥳';
-        bodyElemetn.style.backgroundColor = 'green';
-        number.textContent = secretNumber;
+        // when the userGuess is too high
+        if (userGuess > secretNumber) {
+          message.textContent = 'Too High 💹';
+          currentScore--;
+          score.textContent = currentScore;
+          // when the userGuess is to low
+        } else if (userGuess < secretNumber) {
+          message.textContent = 'Too Low 💹';
+          currentScore--;
+          score.textContent = currentScore;
+        }
+        // when the user wins
+        else {
+          message.textContent = 'Correct Number! 🥳';
+          bodyElemetn.style.backgroundColor = 'green';
+          number.textContent = secretNumber;
+          alreadyGuessed = true;
+        }
       }
     }
   });
 
+// when the user wants to try again
 const again = document.querySelector('.again').addEventListener('click', () => {
   secretNumber = Math.floor(Math.random() * 20 + 1);
   document.querySelector('.guess').value = '';
